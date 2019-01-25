d1f9886a87a578fb04f49bdcf610c3687d5b0955
ReactNativeL <Southidabout27@gmail.com>
2019-01-25 11:33:10 +0700

    Upload already

---
+++ workshop/workshop/src/TextInput.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import {
+    Alert,
+    TextInput,
+    View,
+    Text,
+    Image,
+    TouchableHighlight,
+    StatusBar,
+    KeyboardAvoidingView
+} from 'react-native';
+import { CheckBox } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import mycss from './components/component.js';
+import logo from './img/umoneyL.png';
+import la from './img/la.png';
+import eng from './img/eng.png';
+import mystyle from './components/component.js';
+
+
+export default class MyTextInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isTrue: false,
+            enter_text: '',
+            error_status: true,
+        }
+    }
+
+    Clickme = () => {
+        const { enter_text } = this.state;
+        if (enter_text.trim() != 0) {
+            this.setState({ enter_text: enter_text, error_status: true });
+        } else {
+            this.setState({ enter_text: enter_text, error_status: false });
+        }
+        if (enter_text == "") {
+            Alert.alert("Please enter Phone No");
+            return;
+        }
+        Alert.alert("Hello Number : " + this.state.enter_text);
+    }
+
+    onEnterText = (enter_text) => {
+        if (enter_text.trim() != 0) {
+            this.setState({ enter_text: enter_text, error_status: true });
+        } else {
+            this.setState({ enter_text: enter_text, error_status: false });
+        }
+    }
+
+    render() {
+        return (
+            <KeyboardAvoidingView style={mycss.container}>
+                <StatusBar
+                    backgroundColor="#3bace1"
+                    barStyle="light-content"
+                />
+                <Image source={logo} style={mycss.lognew} />
+                <Text style={mycss.textheader}>u-money</Text>
+                <Text style={mycss.textcontent}>It will give you the convenience of money transfers and payments at any time</Text>
+                <View style={mycss.contentview}>
+                    <Text style={mycss.textphonetitle}>Phone No</Text>
+                    <TextInput placeholder={'Enter Phone No'}
+                        keyboardType={'numeric'}
+                        style={mycss.textNumberInput}
+                        onChangeText={enter_text => this.onEnterText(enter_text)}
+                    // onChangeText={
+                    //     enter_text=> this.setState({enter_text})
+                    // }
+                    />
+                    {
+                        this.state.error_status == false ? (
+                            <Text style={mycss.errorMessage}>
+                                * Please enter Phone No.
+                        </Text>
+                        ) : null
+                    }
+                    <CheckBox
+                        title='Save information'
+                        checked={this.state.isTrue}
+                        style={mycss.checkcontainer}
+                        onPress={() => this.setState({ isTrue: !this.state.isTrue })}
+                        containerStyle={{ marginLeft: 10, marginRight: 20, marginBottom: 20, padding: 0, borderWidth: 0, backgroundColor: 'white' }}
+                        checkedColor={'#9c9d9e'}
+                    />
+                    <View style={mycss.containerTouch}>
+                        <TouchableHighlight>
+                            <Image source={la} style={mycss.lgbtn} />
+                        </TouchableHighlight>
+                        <View><Text>    </Text></View>
+                        <TouchableHighlight>
+                            <Image source={eng} style={mycss.lgbtn} />
+                        </TouchableHighlight>
+                    </View>
+                    <View style={mycss.btnview}>
+                        <TouchableHighlight style={mycss.btnLg} onPress={this.Clickme}>
+                            <Text style={mycss.btnLg_Text}>Sign In</Text>
+                        </TouchableHighlight>
+                    </View>
+                </View>
+
+                <Text style={mycss.textbottom1}>
+                    Verion 1.10.15
+                </Text>
+                <Text style={mycss.textbottom2}>
+                    Oct, 5th 2018
+                </Text>
+            </KeyboardAvoidingView>
+        );
+    }
+}
---
+++ workshop/workshop/src/components/component.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width: WIDTH } = Dimensions.get('window');
+export const IMAGE_HEIGHT = window.width / 2;
+
+const mystyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#3bace1',
+    },
+    logheader: {
+        position: 'absolute',
+        top: 15,
+        alignItems: 'center',
+        width: 120,
+        height: 120
+    },
+
+    lognew:{
+        width: 120,
+        height: 120,
+        resizeMode: 'contain',
+        top: -20,
+        // marginBottom: 20,
+        // padding:10,
+        // marginTop:15
+    },
+    textheader: {
+        fontSize: 30,
+        color: 'white',
+    },
+    textcontent: {
+        width: WIDTH - 60,
+        textAlign: 'center',
+        fontSize: 16,
+        color: 'white',
+        marginBottom: 20
+    },
+    contentview: {
+        width: WIDTH - 55,
+        backgroundColor: 'white',
+        borderRadius: 15,
+        padding: 15
+    },
+    textphonetitle: {
+        marginLeft: 10,
+        color: '#3bace1',
+        fontSize: 16,
+    },
+    textNumberInput: {
+        color: 'black',
+        fontSize: 16,
+        margin: 10,
+        padding: 10,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        borderWidth: 1,
+        height: 45,
+        borderColor: "#cacaca"
+    },
+    checkcontainer: {
+        backgroundColor: 'red',
+        borderColor: "white"
+    },
+    textbottom1: {
+        position: 'absolute',
+        bottom: 25,
+        alignItems: 'center',
+        fontSize: 12,
+        color: 'white',
+    },
+    textbottom2: {
+        position: 'absolute',
+        bottom: 10,
+        alignItems: 'center',
+        fontSize: 12,
+        color: 'white',
+    },
+    lgbtn: {
+        width: 40,
+        height: 30,
+        padding: 10
+    },
+    containerTouch: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    btnLg: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+        width: 250,
+        height: 45,
+        backgroundColor: "#105ab5",
+        borderRadius: 10,
+    },
+    btnLg_Text: {
+        fontSize: 16,
+        color: 'white',
+    },
+    btnview: {
+        marginTop: 10,
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    errorMessage: {
+        fontSize: 16,
+        color: "red",
+        marginLeft:5,
+    }
+});
+
+export default mystyle;
